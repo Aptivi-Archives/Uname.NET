e7b459ffc3411608c716d7b4507956671ab83491
EoflaOE <eoflaoevicecity@gmail.com>
2022-06-14 20:36:06 +0300

    Initial release

---
+++ Uname.NET.Console/Demo.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * MIT License
+ *
+ * Copyright (c) 2022 EoflaOE and its companies
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+
+#if NETCOREAPP
+using System.Runtime.Versioning;
+#endif
+
+namespace UnameNET.Console
+{
+#if NETCOREAPP
+    [UnsupportedOSPlatform("windows")]
+#endif
+    internal class Demo
+    {
+        static void Main()
+        {
+            System.Console.WriteLine("Kernel name:      {0}", UnameManager.GetUname(UnameTypes.KernelName));
+            System.Console.WriteLine("Kernel release:   {0}", UnameManager.GetUname(UnameTypes.KernelRelease));
+            System.Console.WriteLine("Kernel version:   {0}", UnameManager.GetUname(UnameTypes.KernelVersion));
+            System.Console.WriteLine("Network node:     {0}", UnameManager.GetUname(UnameTypes.NetworkNode));
+            System.Console.WriteLine("Machine:          {0}", UnameManager.GetUname(UnameTypes.Machine));
+            System.Console.WriteLine("Operating system: {0}", UnameManager.GetUname(UnameTypes.OperatingSystem));
+        }
+    }
+}
---
+++ Uname.NET/UnameManager.cs
@@ -0,0 +1,94 @@
+﻿/*
+ * MIT License
+ *
+ * Copyright (c) 2022 EoflaOE and its companies
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+
+#if NETCOREAPP
+using System.Runtime.Versioning;
+#endif
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace UnameNET
+{
+#if NETCOREAPP
+	[UnsupportedOSPlatform("windows")]
+#endif
+	public static class UnameManager
+    {
+        /// <summary>
+        /// Gets details from the uname process
+        /// </summary>
+        /// <param name="unameTypes">All the uname types the high-level APIs passed</param>
+        /// <returns>Uname string output</returns>
+        public static string GetUname(UnameTypes unameTypes)
+        {
+			// Check the platform
+			if (Environment.OSVersion.Platform != PlatformID.Unix)
+				throw new PlatformNotSupportedException("This library is only supported on Unix.");
+
+			// Check the uname executable paths
+			string UnameExecutable = File.Exists("/usr/bin/uname")     ? "/usr/bin/uname"     : "/bin/uname";
+		    UnameExecutable        = File.Exists("/system/xbin/uname") ? "/system/xbin/uname" : UnameExecutable;
+
+			// Select arguments according to the types
+			StringBuilder argsBuilder = new();
+			if (unameTypes.HasFlag(UnameTypes.KernelName))
+				argsBuilder.Append("-s ");
+			if (unameTypes.HasFlag(UnameTypes.KernelRelease))
+				argsBuilder.Append("-r ");
+			if (unameTypes.HasFlag(UnameTypes.KernelVersion))
+				argsBuilder.Append("-v ");
+			if (unameTypes.HasFlag(UnameTypes.NetworkNode))
+				argsBuilder.Append("-n ");
+			if (unameTypes.HasFlag(UnameTypes.Machine))
+				argsBuilder.Append("-m ");
+			if (unameTypes.HasFlag(UnameTypes.OperatingSystem))
+				argsBuilder.Append("-o ");
+			string Args = argsBuilder.ToString().Trim();
+
+			// Make a new instance of process
+			Process UnameS = new();
+			ProcessStartInfo UnameSInfo = new()
+			{
+				FileName = UnameExecutable,
+				Arguments = Args,
+				CreateNoWindow = true,
+				UseShellExecute = false,
+				WindowStyle = ProcessWindowStyle.Hidden,
+				RedirectStandardOutput = true
+			};
+			UnameS.StartInfo = UnameSInfo;
+
+			// Start the process and wait for the output to flow
+			UnameS.Start();
+			UnameS.WaitForExit();
+
+			// Return the output
+			return UnameS.StandardOutput.ReadToEnd().Trim(new char[] { '\n' });
+		}
+    }
+}
